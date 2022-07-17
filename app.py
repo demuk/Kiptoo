7f9b91b9ba59195a5ce686278c0b4c1b8183844f
Kiptoo Dennis <denysmutay@gmail.com>
2022-07-17 21:44:49 +0300

    routes for bookorama,pointofsale

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Message, Mail
+import uuid
 
 
 app = Flask(__name__)
@@ -10,21 +11,30 @@ app = Flask(__name__)
 #                   MAIL_PORT=587, MAIL_USE_SSL=False, MAIL_USE_TLS=True, MAIL_USERNAME='kdennsmutai@gmail.com',
 #                   MAIL_PASSWORD="KIPTOODENNIS123")
 # mail = Mail(app)
+#
+# app.config['MAIL_SERVER']='smtp.mailtrap.io'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'a01094e891d635'
+# app.config['MAIL_PASSWORD'] = '86c28bc9a8074e'
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = False
 
-app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
-app.config['MAIL_SERVER'] = 'smtp.office365.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'mtydnys@outlook.com'
-app.config['MAIL_PASSWORD'] = 'KIPTOODENNIS123'
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = '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'
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 mail = Mail(app)
+
+
 
 
 @app.route('/home')
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',id=uuid.uuid4())
 
 
 @app.route('/policy')
@@ -46,11 +56,18 @@ def portfolio():
 def kapsuchai():
     return render_template('kapsuchai.html')
 
+@app.route('/pointofsale')
+def pointofsale():
+    return render_template('pointofsale.html')
+
 
 @app.route('/darthorama')
 def darthorama():
     return render_template('darthorama.html')
 
+@app.route('/bookorama')
+def bookorama():
+    return render_template('bookorama.html')
 
 @app.route('/projects')
 def projects():
@@ -74,8 +91,8 @@ def contact():
         email = request.form.get('email')
         subject = request.form.get('subject')
         message = request.form.get('message')
-        sender = 'mtydnys@outlook.com'
-        recipients = ['mtydnys@outlook.com']
+        sender = 'denysmutay@gmail.com'
+        recipients = ['denysmutay@gmail.com']
         msg = Message(body=f"Name: {name}\n EMail: {email}\n Subject:{subject}\n Message:{message}",
                       recipients=recipients, sender=sender)
         mail.send(msg)
